65ccb0f57366e2b8514f38176d4d082af1c42c59
chenpeiyuan1987 <chenpeiyuan1987@163.com>
2017-08-05 00:11:06 +0800

    Adding XmlBeanFactory

--- src/main/java/org/yuan/study/spring/util/Assert.java
+++ src/main/java/org/yuan/study/spring/util/Assert.java
@@ -1,11 +1,76 @@
 package org.yuan.study.spring.util;
 
+/**
+ * Assertion utility class that assists in validating arguments.
+ *
+ */
 public abstract class Assert {
 
 	/**
+	 * Assert a boolean expression, throwing IllegalArgumentException
+	 * if the test result is false.
+	 * @param expression
+	 * @param message
+	 */
+	public static void isTrue(boolean expression, String message) {
+		if (!expression) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	/**
+	 * Assert a boolean expression, throwing IllegalArgumentException
+	 * if the test result is false.
+	 * @param expression
+	 */
+	public static void isTrue(boolean expression) {
+		isTrue(expression, "[Assertion failed] - this expression must be true.");
+	}
+	
+	/**
+	 * Assert that an object is null.
+	 * @param object
+	 * @param message
+	 */
+	public static void isNull(Object object, String message) {
+		if (object != null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	/**
+	 * Assert that an object is null.
+	 * @param object
+	 */
+	public static void isNull(Object object) {
+		isNull(object, "[Assertion failed] - the object argument must be null.");
+	}
+	
+	/**
+	 * Assert that a string is not empty;
+	 * It must not be null and not empty.
+	 * @param text
+	 * @param message
+	 */
+	public static void hasLength(String text, String message) {
+		if (!StringUtils.hasLength(text)) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	/**
+	 * Assert that a string is not empty;
+	 * It must not be null and not empty.
+	 * @param text
+	 */
+	public static void hasLength(String text) {
+		hasLength(text, "[Assertion failed] - this String argument must have length; it must not be null or empty. ");
+	}
+	
+	/**
 	 * Assert that a string has valid text content.
 	 * It must not be null and must contain at least one non-whitespace character.
-	 * @param str
+	 * @param text
 	 * @param message
 	 */
 	public static void hasText(String text, String message) {
@@ -17,8 +82,7 @@ public abstract class Assert {
 	/**
 	 * Assert that a string has valid text content.
 	 * It must not be null and must contain at least one non-whitespace character.
-	 * @param str
-	 * @param message
+	 * @param text
 	 */
 	public static void hasText(String text) {
 		hasText(text, "[Assertion failed] - this String argument must have text; it must not be null, empty or blank.");
@@ -38,12 +102,23 @@ public abstract class Assert {
 	/**
 	 * Assert that an object is not null.
 	 * @param object
-	 * @param message
 	 */
 	public static void notNull(Object object) {
 		notNull(object, "[Assertion failed] - this argument is required; it cannot be null.");
 	}
 	
+	/**
+	 * Assert that the provided object is an instance of the provided class.
+	 * @param clazz
+	 * @param object
+	 */
+	public static void isInstanceOf(Class<?> clazz, Object object) {
+		isInstanceOf(clazz, object, "");
+	}
+	
+	public static void isInstanceOf(Class<?> clazz, Object object, String message) {
+		// TODO
+	}
 	/**
 	 * Assert a boolean expression, throwing IllegalStateException
 	 * if the test result is false.
