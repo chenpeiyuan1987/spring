e0f5d5900b468621da41385071eadaac5bc01f7c
chenpeiyuan1987 <chenpeiyuan1987@aliyun.com>
2017-10-03 12:12:14 +0800

    Add CollectionFactory、LinkedCaseInsensitiveMap

--- src/main/java/org/yuan/study/spring/core/CollectionFactory.java
+++ src/main/java/org/yuan/study/spring/core/CollectionFactory.java
@@ -1,94 +1,297 @@
 package org.yuan.study.spring.core;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.yuan.study.spring.util.LinkedCaseInsensitiveMap;
 
+/**
+ * Factory for collections, being aware of Java 5 and Java 6 collections.
+ * Mainly for internal use within the framworkk.
+ * 
+ * The goal of this class is to avoid runtime dependencies on a specific
+ * Java version, while nevertheless using the best collection implementation
+ * that is available at runtime.
+ * 
+ * @author Yuan
+ *
+ */
 public abstract class CollectionFactory {
+	
+	private static Class navigableSetClass = null;
+	
+	private static Class navigableMapClass = null;
+	
+	private static final Set<Class> approximableCollectionTypes = new HashSet<Class>(10);
+	
+	private static final Set<Class> approximableMapTypes = new HashSet<Class>(6);
 
-	private static final String COMMONS_COLLECTIONS_CLASS_NAME = 
-		"org.apache.commons.collections.map.LinkedMap";
-	
-	private static final Log logger = LogFactory.getLog(CollectionFactory.class);
-	
-	private static boolean commonsCollections3xAvailable;
-	
 	static {
-		if (JdkVersion.getMajorJavaVersion() >= JdkVersion.JAVA_14) {
-			logger.info("JDK 1.4+ collections available");
-		}
+		approximableCollectionTypes.add(Collection.class);
+		approximableCollectionTypes.add(List.class);
+		approximableCollectionTypes.add(Set.class);
+		approximableCollectionTypes.add(SortedSet.class);
+		approximableMapTypes.add(Map.class);
+		approximableMapTypes.add(SortedMap.class);
+		
+		ClassLoader cl = CollectionFactory.class.getClassLoader();
 		try {
-			Class.forName(COMMONS_COLLECTIONS_CLASS_NAME);
-			commonsCollections3xAvailable = true;
-			logger.info("Commons Collections 3.x available");
+			navigableSetClass = cl.loadClass("java.util.NavigableSet");
+			navigableMapClass = cl.loadClass("java.util.NavigableMap");
+			approximableCollectionTypes.add(navigableSetClass);
+			approximableMapTypes.add(navigableMapClass);
 		}
 		catch (ClassNotFoundException ex) {
-			commonsCollections3xAvailable = false;
+			
+		}
+		
+		approximableCollectionTypes.add(ArrayList.class);
+		approximableCollectionTypes.add(LinkedList.class);
+		approximableCollectionTypes.add(HashSet.class);
+		approximableCollectionTypes.add(LinkedHashSet.class);
+		approximableCollectionTypes.add(TreeSet.class);
+		approximableMapTypes.add(HashMap.class);
+		approximableMapTypes.add(LinkedHashMap.class);
+		approximableMapTypes.add(TreeMap.class);
+	}
+	
+	/**
+	 * Create a linked Set if possible: This implementation always
+	 * creates a java.util.LinkedHashSet, since Spring 2.5 requires
+	 * JDK 1.4 anyway.
+	 * @param initial
+	 * @return
+	 */
+	@Deprecated
+	public static <T> Set<T> createLinkedSetIfPossible(int initial) {
+		return new LinkedHashSet<T>(initial);
+	}
+	
+	/**
+	 * Create a copy-on-write Set (allowing for synchronization-less iteration) if possible:
+	 * This implementation always creates a java.util.concurrent.CopyOnWriteArraySet,
+	 * since Spring 3 requires JDK 1.5 anyway.
+	 * @return
+	 */
+	@Deprecated
+	public static <T> Set<T> createCopyOnWriteSet() {
+		return new CopyOnWriteArraySet<T>();
+	}
+	
+	/**
+	 * Create a linked Map if possible: This implementation always
+	 * creates a java.util.LinkedHashMap, since Spring 2.5 requires
+	 * JDK 1.4 anyway.
+	 * @param initial
+	 * @return
+	 */
+	@Deprecated
+	public static <K,V> Map<K,V> createLinkedMapIfPossible(int initial) {
+		return new LinkedHashMap<K,V>(initial);
+	}
+	
+	/**
+	 * Create a linked case-insensitive Map if possible: This implementation
+	 * always returns a org.springframework.util.LinkedCaseInsensitiveMap.
+	 * @param initial
+	 * @return
+	 */
+	@Deprecated
+	public static <V> Map<String,V> createLinkedCaseInsensitiveMapIfPossible(int initial) {
+		return new LinkedCaseInsensitiveMap<V>(initial);
+	}
+	
+	/**
+	 * Create an identity Map if possible: This implementation always
+	 * creates a java.util.IdentityHashMap, since Spring 2.5 requires 
+	 * JDK 1.4 anyway.
+	 * @param initial
+	 * @return
+	 */
+	@Deprecated
+	public static <K,V> Map<K,V> createIdentityMapIfPossible(int initial) {
+		return new IdentityHashMap<K,V>(initial);
+	}
+	
+	/**
+	 * Create a concurrent Map if possible: This implementation always
+	 * creates a java.util.concurrent.ConcurrentHashMap, since Spring 
+	 * 3.0 requires JDK 1.5 anyway.
+	 * @param initial
+	 * @return
+	 */
+	public static <K,V> Map<K,V> createConcurrentMapIfPossible(int initial) {
+		return new ConcurrentHashMap<K,V>(initial);
+	}
+	
+	/**
+	 * Create a concurrent Map with a dedicated ConcurrentMap interface:
+	 * This implementation always creates a java.util.concurrent.ConcurrentHashMap,
+	 * since Spring 3.0 requires JDK 1.5 anyway.
+	 * @param initial
+	 * @return
+	 */
+	@Deprecated
+	public static <K,V> ConcurrentHashMap<K,V> createConcurrentMap(int initial) {
+		return new JdkConcurrentHashMap<K,V>(initial);
+	}
+	
+	/**
+	 * Determine whether the given collection type is an approximable type,
+	 * i.e. a type that can approximate.
+	 * @param collectionType
+	 * @return
+	 */
+	public static boolean isApproximableCollectionType(Class<?> collectionType) {
+		return (collectionType != null && approximableCollectionTypes.contains(collectionType));
+	}
+	
+	/**
+	 * Create the most approximate collection for the given collection.
+	 * Creates an ArrayList, TreeSet or linked Set for a List, SortedSet
+	 * or Set, respectively.
+	 * @param collection
+	 * @param initial
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> Collection<T> createApproximateCollection(Object collection, int initial) {
+		if (collection instanceof LinkedList) {
+			return new LinkedList<T>();
+		}
+		else if (collection instanceof List) {
+			return new ArrayList<T>(initial);
+		}
+		else if (collection instanceof SortedSet) {
+			return new TreeSet<T>(((SortedSet<T>) collection).comparator());
+		}
+		else {
+			return new LinkedHashSet<T>(initial);
 		}
 	}
 	
 	/**
-	 * Create a linked set if possible: that is, if running on JDK >= 1.4
-	 * or if Commons Collections 3.x is available.
-	 * @param initialCapacity
+	 * Create the most appropriate collection for the given collection type.
+	 * Creates an ArrayList, TreeSet or linked Set for a List, SortedSet
+	 * or Set, respectively.
+	 * @param collectionType
+	 * @param initial
 	 * @return
 	 */
-	public static Set<?> createLinkedSetIfPossible(int initial) {
-		return new HashSet(initial);
-	}
-	
-	/**
-	 * Create a linked map if possible: that is, if running on JDK >= 1.4
-	 * or if Commons Collections 3.x is available.
-	 * @param initialCapacity
-	 * @return
-	 */
-	public static Map<?,?> createLinkedMaoIfPossible(int initial) {
-		return new HashMap(initial);
-	}
-	
-	/**
-	 * Create a linked case-insensitive map if possible.
-	 * @param initialCapacity
-	 * @return
-	 */
-	public static Map<?,?> createLinkedCaseInsensitiveMapIfPossible(int initial) {
-		return new HashMap();
-	}
-	
-	/**
-	 * Create ab identity map if possible.
-	 * @param initialCapacity
-	 * @return
-	 */
-	public static Map<?,?> createIdentityMapIfPossible(int initial) {
-		return new HashMap(initial);
-	}
-	
-	/**
-	 * Actual creation of JDK 1.4+ Collections.
-	 */
-	private static abstract class Jdk14CollectionFactory {
-		
-		private static Set createLinkedHashSet(int initial) {
-			return new LinkedHashSet(initial);
+	@SuppressWarnings("unchecked")
+	public static <T> Collection<T> createCollection(Class<?> collectionType, int initial) {
+		if (collectionType.isInterface()) {
+			if (List.class.equals(collectionType)) {
+				return new ArrayList<T>(initial);
+			} 
+			else if (SortedSet.class.equals(collectionType) || collectionType.equals(navigableSetClass)) {
+				return new TreeSet<T>();
+			}
+			else if (Set.class.equals(collectionType) || Collection.class.equals(collectionType)) {
+				return new LinkedHashSet<T>(initial);
+			}
+			else {
+				throw new IllegalArgumentException("Unsupported Collection interface: " + collectionType.getName());
+			}
+		} 
+		else {
+			if (!Collection.class.isAssignableFrom(collectionType)) {
+				throw new IllegalArgumentException("Unsupported Collection type: " + collectionType.getName());
+			}
+			try {
+				return (Collection<T>) collectionType.newInstance();
+			}
+			catch (Exception ex) {
+				throw new IllegalArgumentException("Could not instantiate Collection type: " + collectionType.getName());
+			}
 		}
-		
-		private static Map createLinkedHashMap(int initial) {
-			return new LinkedHashMap(initial);
-		}
-		
-		private static Map createIdentityHashMap(int initial) {
-			return new IdentityHashMap(initial);
-		}
-		
 	}
 	
+	/**
+	 * Determine whether the given map type is an approximable type,
+	 * i.e. a type that can approximate.
+	 * @param mapType
+	 * @return
+	 */
+	public static boolean isApproximableMapType(Class<?> mapType) {
+		return (mapType != null && approximableMapTypes.contains(mapType));
+	}
+	
+	/**
+	 * Create the most approximate map for the given map.
+	 * Creates a TreeMap or linked Map for a SortedMap or Map,
+	 * respectively.
+	 * @param map
+	 * @param initial
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <K,V> Map<K,V> createApproximateMap(Object map, int initial) {
+		if (map instanceof SortedMap) {
+			return new TreeMap<K,V>(((SortedMap<K,V>) map).comparator());
+		}
+		else {
+			return new LinkedHashMap<K,V>(initial);
+		}
+	}
+	
+	/**
+	 * Create the msot approximate map for the given map.
+	 * Creates a TreeMap or linked Map for a SortedMap or Map, 
+	 * respectively.
+	 * @param mapType
+	 * @param initial
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <K,V> Map<K,V> createMap(Class<?> mapType, int initial) {
+		if (mapType.isInterface()) {
+			if (Map.class.equals(mapType)) {
+				return new LinkedHashMap<K,V>(initial);
+			} 
+			else if (SortedMap.class.equals(mapType) || mapType.equals(navigableMapClass)) {
+				return new TreeMap<K,V>();
+			}
+			else {
+				throw new IllegalArgumentException("Unsupported Map interface: " + mapType.getName());
+			}
+		} 
+		else {
+			if (!Map.class.isAssignableFrom(mapType)) {
+				throw new IllegalArgumentException("Unsupported Map type: " + mapType.getName());
+			}
+			try {
+				return (Map<K,V>) mapType.newInstance();
+			}
+			catch (Exception ex) {
+				throw new IllegalArgumentException("Could not instantiate Map type: " + mapType.getName());
+			}
+		}
+	}
+	
+	/**
+	 * ConcurrentMap adapter for the JDK ConcurrentHashMap class.
+	 */
+	@Deprecated
+	private static class JdkConcurrentHashMap<K,V> extends ConcurrentHashMap<K,V> implements ConcurrentMap<K,V> {
+		private JdkConcurrentHashMap(int initial) {
+			super(initial);
+		}
+	}
 }
---
+++ src/main/java/org/yuan/study/spring/util/LinkedCaseInsensitiveMap.java
@@ -0,0 +1,108 @@
+package org.yuan.study.spring.util;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * variant that stores String keys in a case-insensitive
+ * manner, for example for key-based access in a results table.
+ * 
+ * Preserves the original order as well as the original casing of keys,
+ * while allowing for contains, get and remove calls with any case of key.
+ * 
+ * Does not support null keys.
+ * 
+ * @author Yuan
+ */
+public class LinkedCaseInsensitiveMap<V> extends LinkedHashMap<String, V> {
+	private static final long serialVersionUID = 1L;
+
+	private final Locale locale;
+
+	/**
+	 * Create a new LinkedCaseInsensitiveMap for the default Locale.
+	 */
+	public LinkedCaseInsensitiveMap() {
+		this(null);
+	}
+
+	/**
+	 * Create a new LinkedCaseInsensitiveMap that stores lower-case keys
+	 * according to the given Locale.
+	 * @param locale
+	 */
+	public LinkedCaseInsensitiveMap(Locale locale) {
+		super();
+		this.locale = (locale != null ? locale : Locale.getDefault());
+	}
+	
+	/**
+	 * Create a new LinkedCaseInsensitiveMap that wraps a LinkedHashMap
+	 * with the given initial capacity and stores lower-case keys according
+	 * to the default Locale.
+	 * @param initial
+	 */
+	public LinkedCaseInsensitiveMap(int initial) {
+		this(initial, null);
+	}
+	
+	/**
+	 * Create a new LinkedCaseInsensitiveMap that wraps a LinkedHashMap
+	 * with the given initial capacity and stores lower-case keys according
+	 * to the given Locale.
+	 * @param initial
+	 * @param locale
+	 */
+	public LinkedCaseInsensitiveMap(int initial, Locale locale) {
+		super(initial);
+		this.locale = (locale != null ? locale : Locale.getDefault());
+	}
+
+	@Override
+	public V get(Object key) {
+		if (key instanceof String) {
+			return super.get(convertKey((String) key));
+		} 
+		else {
+			return null;
+		}
+	}
+
+	@Override
+	public void clear() {
+		super.clear();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return (key instanceof String && this.containsKey(key));
+	}
+
+	@Override
+	public V put(String key, V value) {
+		return super.put(convertKey(key), value);
+	}
+
+	@Override
+	public V remove(Object key) {
+		if (key instanceof String) {
+			return super.remove(convertKey((String)key));
+		} 
+		else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Convert the given key to case-insensitive key.
+	 * The default implementation converts the key to 
+	 * lower-case according to this Map's Locale.
+	 * @param key
+	 * @return
+	 */
+	protected String convertKey(String key) {
+		return key.toLowerCase(this.locale);
+	}
+}
