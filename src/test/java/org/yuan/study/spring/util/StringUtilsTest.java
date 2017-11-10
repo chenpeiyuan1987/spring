13e26514121e8db774892596caee5bf4be69d060
chenpeiyuan <chenpeiyuan1987@163.com>
2017-11-10 15:59:38 +0800

    Tested StringUtils

--- src/test/java/org/yuan/study/spring/util/StringUtilsTest.java
+++ src/test/java/org/yuan/study/spring/util/StringUtilsTest.java
@@ -1,13 +1,16 @@
 package org.yuan.study.spring.util;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Locale;
+import java.util.Properties;
 
 import org.junit.Test;
 
@@ -283,37 +286,85 @@ public class StringUtilsTest {
 	
 	@Test
 	public void testPathEquals() {
-		
+		assertTrue(StringUtils.pathEquals("/dummy1/dummy2/dummy3", "/dummy1/dummy2/dummy3"));
+		assertTrue(StringUtils.pathEquals("C:\\dummy1\\dummy2\\dummy3", "C:\\dummy1\\dummy2\\dummy3"));
+		assertTrue(StringUtils.pathEquals("/dummy1/bin/../dummy2/dummy3", "/dummy1/dummy2/dummy3"));
+		assertTrue(StringUtils.pathEquals("C:\\dummy1\\dummy2\\dummy3", "C:\\dummy1\\bin\\..\\dummy2\\dummy3"));
+		assertTrue(StringUtils.pathEquals("/dummy1/bin/../dummy2/bin/../dummy3", "/dummy1/dummy2/dummy3"));
+		assertTrue(StringUtils.pathEquals("C:\\dummy1\\dummy2\\dummy3", "C:\\dummy1\\bin\\..\\dummy2\\bin\\..\\dummy3"));
+		assertTrue(StringUtils.pathEquals("/dummy1/bin/tmp/../../dummy2/dummy3", "/dummy1/dummy2/dummy3"));
+		assertTrue(StringUtils.pathEquals("/dummy1/dummy2/dummy3", "/dummy1/dum/dum/../../dummy2/dummy3"));
+		assertTrue(StringUtils.pathEquals("./dummy1/dummy2/dummy3", "dummy1/dum/./dum/../../dummy2/dummy3"));
+		assertFalse(StringUtils.pathEquals("./dummy1/dummy2/dummy3", "/dummy1/dum/./dum/../../dummy2/dummy3"));
+		assertFalse(StringUtils.pathEquals("/dummy1/dummy2/dummy3", "/dummy1/dummy4/dummy3"));
+		assertFalse(StringUtils.pathEquals("/dummy1/bin/tmp/../dummy2/dummy3", "/dummy1/dummy2/dummy3"));
+		assertFalse(StringUtils.pathEquals("C:\\dummy1\\dummy2\\dummy3", "C:\\dummy1\\bin\\tmp\\..\\dummy2\\dummy3"));
+		assertFalse(StringUtils.pathEquals("/dummy1/bin/../dummy2/dummy3", "/dummy1/dummy2/dummy4"));
 	}
 	
 	@Test
 	public void testConcatenateStringArrays() {
+		String[] input1 = new String[] {"myString2"};
+		String[] input2 = new String[] {"myString1", "myString2"};
+		String[] result = StringUtils.concatenateStringArrays(input1, input2);
+		assertEquals(3, result.length);
+		assertEquals("myString2", result[0]);
+		assertEquals("myString1", result[1]);
+		assertEquals("myString2", result[2]);
 		
+		assertNull(StringUtils.concatenateStringArrays(null, null));
+		assertArrayEquals(input1, StringUtils.concatenateStringArrays(input1, null));
+		assertArrayEquals(input2, StringUtils.concatenateStringArrays(null, input2));
 	}
 	
 	@Test
 	public void testMergeStringArrays() {
+		String[] input1 = new String[] {"myString2"};
+		String[] input2 = new String[] {"myString1", "myString2"};
+		String[] result = StringUtils.mergeStringArrays(input1, input2);
+		assertEquals(2, result.length);
+		assertEquals("myString2", result[0]);
+		assertEquals("myString1", result[1]);
 		
+		assertNull(StringUtils.mergeStringArrays(null, null));
+		assertArrayEquals(input1, StringUtils.mergeStringArrays(input1, null));
+		assertArrayEquals(input2, StringUtils.mergeStringArrays(null, input2));
 	}
 	
 	@Test
 	public void testSortStringArray() {
+		String[] input = new String[] {"myString2"};
+		input = StringUtils.addStringToArray(input, "myString1");
+		assertEquals("myString2", input[0]);
+		assertEquals("myString1", input[1]);
 		
+		StringUtils.sortStringArray(input);
+		assertEquals("myString1", input[0]);
+		assertEquals("myString2", input[1]);
 	}
 	
 	@Test
 	public void testRemoveDuplicateStrings() {
-		
+		String[] input = new String[] {"myString2", "myString1", "myString2"};
+		input = StringUtils.removeDuplicateStrings(input);
+		assertEquals("myString1", input[0]);
+		assertEquals("myString2", input[1]);
 	}
 	
 	@Test
 	public void testSplitArrayElementsIntoProperties() {
-		
+		String[] input = new String[] {"key1=value1", "key2 =\"value2\""};
+		Properties result = StringUtils.splitArrayElementsIntoProperties(input, "=");
+		assertEquals("value1", result.getProperty("key1"));
+		assertEquals("\"value2\"", result.getProperty("key2"));
 	}
 	
 	@Test
 	public void testSplitArrayElementsIntoPropertiesAndDeletedChars() {
-		
+		String[] input = new String[] {"key1=value1", "key2 =\"value2\""};
+		Properties result = StringUtils.splitArrayElementsIntoProperties(input, "=", "\"");
+		assertEquals("value1", result.getProperty("key1"));
+		assertEquals("value2", result.getProperty("key2"));
 	}
 	
 	@Test
@@ -398,7 +449,151 @@ public class StringUtilsTest {
 	
 	@Test
 	public void testCommaDelimitedListToStringArrayMatchWords() {
+		String[] sa = new String[] {"foo", "bar", "big"};
+		doTestCommaDelimitedListToStringArrayLegalMatch(sa);
+		testStringArrayReverseTransformationMatches(sa);
 		
+		sa = new String[] {"a", "b", "c"};
+		doTestCommaDelimitedListToStringArrayLegalMatch(sa);
+		testStringArrayReverseTransformationMatches(sa);
+		
+		sa = new String[] {"AA", "AA", "AA", "AA", "AA"};
+		doTestCommaDelimitedListToStringArrayLegalMatch(sa);
+		testStringArrayReverseTransformationMatches(sa);
 	}
 	
+	@Test
+	public void testCommaDelimitedListToStringArraySingleString() {
+		String s = "woeirqupoiewuropqiewuorpqiwueopriquwopeiurqopwieur";
+		String[] sa = StringUtils.commaDelimitedListToStringArray(s);
+		assertTrue(sa.length == 1);
+		assertTrue(sa[0].equals(s));
+	}
+	
+	@Test
+	public void testCommaDelimitedListToStringArrayWithOtherPunctuation() {
+		String[] sa = new String[] {"xcvwert4456346&*.", "///", ".!", ".", ";"};
+		doTestCommaDelimitedListToStringArrayLegalMatch(sa);
+	}
+	
+	@Test
+	public void testCommaDelimitedListToStringArrayEmptyStrings() {
+		String[] sa = StringUtils.commaDelimitedListToStringArray("a,,b");
+		assertEquals(3, sa.length);
+		assertTrue(sa[0].equals("a"));
+		assertTrue(sa[1].equals(""));
+		assertTrue(sa[2].equals("b"));
+		
+		sa = new String[] {"", "", "a", ""};
+		doTestCommaDelimitedListToStringArrayLegalMatch(sa);
+	}
+	
+	private void doTestCommaDelimitedListToStringArrayLegalMatch(String[] components) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < components.length; i++) {
+			if (i > 0) {
+				sb.append(",");
+			}
+			sb.append(components[i]);
+		}
+		
+		String[] sa = StringUtils.commaDelimitedListToStringArray(sb.toString());
+		assertTrue(sa != null);
+		assertTrue(components.length == sa.length);
+		assertTrue(Arrays.equals(sa, components));
+	}
+	
+	@Test
+	public void testEndsWithIgnoreCase() {
+		String suffix = "fOo";
+		assertTrue(StringUtils.endsWithIgnoreCase("foo", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("Foo", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barfoo", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barbarfoo", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barFoo", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barBarFoo", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barfoO", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barFOO", suffix));
+		assertTrue(StringUtils.endsWithIgnoreCase("barfOo", suffix));
+		assertFalse(StringUtils.endsWithIgnoreCase(null, suffix));
+		assertFalse(StringUtils.endsWithIgnoreCase("barfOo", null));
+		assertFalse(StringUtils.endsWithIgnoreCase("b", suffix));
+	}
+	
+	@Test
+	public void testParseLocaleStringSunnyDay() {
+		Locale expectedLocale = Locale.UK;
+		Locale locale = StringUtils.parseLocaleString(expectedLocale.toString());
+		assertNotNull(locale);
+		assertEquals(expectedLocale, locale);
+	}
+	
+	@Test
+	public void testParseLocaleStringWithMalformedLocaleString() {
+		Locale locale = StringUtils.parseLocaleString("_banjo_on_my_knee");
+		assertNotNull(locale);
+	}
+	
+	@Test
+	public void testParseLocaleStringWithEmptyLocaleStringYieldsNullLocale() {
+		Locale locale = StringUtils.parseLocaleString("");
+		assertNull(locale);
+	}
+	
+	@Test
+	public void testParseLocaleWithMultiSpecialCharactersInVariant() {
+		String variant = "proper-northern";
+		String localeString = "en_GB_" + variant;
+		Locale locale = StringUtils.parseLocaleString(localeString);
+		assertEquals(variant, locale.getVariant());
+	}
+	
+	@Test
+	public void testParseLocaleWithMultiValuedVariant() {
+		String variant = "proper_northern";
+		String localeString = "en_GB_" + variant;
+		Locale locale = StringUtils.parseLocaleString(localeString);
+		assertEquals(variant, locale.getVariant());
+	}
+	
+	@Test
+	public void testParseLocaleWithMultiValuedVariantUsingSpacesAsSeparators() {
+		String variant = "proper northern";
+		String localeString = "en GB " + variant;
+		Locale locale = StringUtils.parseLocaleString(localeString);
+		assertEquals(variant, locale.getVariant());
+	}
+	
+	@Test
+	public void testParseLocaleWithMultiValuedVariantUsingMixtureOfUnderscoresAndSpacesAsSeparators() {
+		String variant = "proper northern";
+		String localeString = "en_GB_" + variant;
+		Locale locale = StringUtils.parseLocaleString(localeString);
+		assertEquals(variant, locale.getVariant());
+	}
+	
+	@Test
+	public void testParseLocaleWithMultiValuedVariantUsingSpacesAsSeparatorsWithLotsOfLeadingWhitespace() {
+		String variant = "proper northern";
+		String localeString = "en GB             " + variant;
+		Locale locale = StringUtils.parseLocaleString(localeString);
+		assertEquals(variant, locale.getVariant());
+	}
+	
+	@Test
+	public void testParseLocaleWithMultiValuedVariantUsingUnderscoresAsSeparatorsWithLotsOfLeadingWhitespace() {
+		String variant = "proper_northern";
+		String localeString = "en_GB______" + variant;
+		Locale locale = StringUtils.parseLocaleString(localeString);
+		assertEquals(variant, locale.getVariant());
+	}
+	
+	@Test
+	public void testParseLocaleWithInvalidCharacters() {
+		try {
+			StringUtils.parseLocaleString("%0D%0AContent-length:30%0D%0A%0D%0A%3Cscript%3Ealert%28123%29%3C/script%3E");
+			fail("Should have thrown IllegalArgumentException");
+		}
+		catch (IllegalArgumentException ex) {}
+	}
 }
