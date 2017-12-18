2aa4056b14977df2bc15d8290c6bd4e05eb77b63
chenpeiyuan1987 <chenpeiyuan1987@163.com>
2017-12-19 00:40:46 +0800

    Adding AbstractAutowireCapableBeanFactory

--- src/main/java/org/yuan/study/spring/beans/factory/support/ChildBeanDefinition.java
+++ src/main/java/org/yuan/study/spring/beans/factory/support/ChildBeanDefinition.java
@@ -1,11 +1,14 @@
 package org.yuan.study.spring.beans.factory.support;
 
 import org.yuan.study.spring.beans.MutablePropertyValues;
+import org.yuan.study.spring.beans.factory.config.BeanDefinition;
 import org.yuan.study.spring.beans.factory.config.ConstructorArgumentValues;
+import org.yuan.study.spring.util.ObjectUtils;
 
 public class ChildBeanDefinition extends AbstractBeanDefinition {
+	private static final long serialVersionUID = 1L;
 	
-	private final String parentName;
+	private String parentName;
 
 	/**
 	 * Create a new ChildBeanDefinition for the given parent.
@@ -65,10 +68,18 @@ public class ChildBeanDefinition extends AbstractBeanDefinition {
 		setBeanClassName(beanClassName);
 	}
 
-	public String getParentName() {
-		return parentName;
+	/**
+	 * Create a new ChildBeanDefinition for the given parent,
+	 * providing  constructor arguments and property values.
+	 * @param parentName
+	 * @param beanClassName
+	 * @param cargs
+	 * @param pvs
+	 */
+	public ChildBeanDefinition(ChildBeanDefinition original) {
+		super((BeanDefinition) original);
 	}
-
+	
 	//---------------------------------------------------------------
 	// Implementation of AbstractBeanDefinition class
 	//---------------------------------------------------------------
@@ -82,10 +93,43 @@ public class ChildBeanDefinition extends AbstractBeanDefinition {
 	}
 
 	@Override
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+	
+
+	@Override
+	public AbstractBeanDefinition cloneBeanDefinition() {
+		return new ChildBeanDefinition(this);
+	}
+	
+
+	@Override
+	public int hashCode() {
+		return ObjectUtils.nullSafeHashCode(parentName) * 29 + super.hashCode();
+	}
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof ChildBeanDefinition)) {
+			return false;
+		}
+		ChildBeanDefinition other = (ChildBeanDefinition) obj;
+		return ObjectUtils.nullSafeEquals(parentName, other.parentName) && super.equals(obj);
+	}
+	
+	@Override
 	public String toString() {
 		StringBuffer sb = new StringBuffer("Child bean with parent '");
 		sb.append(this.parentName).append("': ").append(super.toString());
 		return sb.toString();
 	}
 	
+	public String getParentName() {
+		return parentName;
+	}
 }
